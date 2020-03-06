636be4885ad770c5bb266fb018362fc41df907d9
Mukeshkumar2 <59080342+Mukeshkumar2@users.noreply.github.com>
2020-03-06 19:39:11 +0530

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws s3 --region us-east-1 cp s3://mkpbucket/django_scripts/py.sh /var/www/basic_app/
-sh /var/www/basic_app/py.sh
+aws s3 --region us-east-1 cp s3://mkpbucket/django_scripts/python.sh /var/www/basic_app/
+sh /var/www/basic_app/python.sh
